ce21955b74055ca8c7d5e6b6ab389980a87ced71
Antonio Muñiz <amunizmartin@gmail.com>
2014-06-16 21:25:22 +0200

    Formatting source code

--- github-pr-demo/src/main/java/es/clinker/demo/App.java
+++ github-pr-demo/src/main/java/es/clinker/demo/App.java
@@ -7,7 +7,7 @@ package es.clinker.demo;
 public class App 
 {
     public static void main(String[] args) {
-        for(int i = 0; i < 2; i++) {
+        or(int i = 0; i < 2; i++) {
             System.out.println("Hello World " + i + "!");
         }
     }
